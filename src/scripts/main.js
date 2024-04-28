138698d350884c05fbf31b89352ace668adcdbee
Alan Campos <alancamposdeveloper@gmail.com>
2024-04-28 15:55:45 -0400

    finalizando o header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,24 @@
 
+
+/*############################################
+
+function convertCurrency() {
+  let amount = parseFloat(document.getElementById('amount').value);
+  let fromCurrency = document.getElementById('fromCurrency').value;
+  let toCurrency = document.getElementById('toCurrency').value;
+
+  // Implemente a lógica de conversão aqui
+
+  let result = amount * conversionRate; // Substitua conversionRate pelo valor correto
+
+  document.getElementById('result').textContent = `${amount} ${fromCurrency} equivalem a ${result} ${toCurrency}`;
+}
+ 
+*/
+
+
+
+
 // Atualização do ano no rodapé
 function currentYear(){
   const year = new Date().getFullYear();
